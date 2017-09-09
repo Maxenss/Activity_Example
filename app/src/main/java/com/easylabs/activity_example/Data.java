8571f3b37d9f28355d35b39292cf5c01cf061b49
Maxenss <kokayni4@gmail.com>
2017-09-09 15:21:37 +0300

    Second

--- app/src/main/java/com/easylabs/activity_example/Data.java
+++ app/src/main/java/com/easylabs/activity_example/Data.java
@@ -2,6 +2,11 @@ package com.easylabs.activity_example;
 
 
 public class Data {
+    // Ключи для Intent
     static final String KEY_LOGIN = "login";
     static final String KEY_PASSWORD = "password";
+    // Ключи для SP
+    static final String SP_KEY_LOGIN = "sp_login";
+    static final String SP_KEY_PASSWORD = "sp_password";
+    static final String SP_KEY_USERS = "sp_password";
 }
--- app/src/main/java/com/easylabs/activity_example/MainActivity.java
+++ app/src/main/java/com/easylabs/activity_example/MainActivity.java
@@ -1,18 +1,22 @@
 package com.easylabs.activity_example;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText etLogin;
     EditText etPassword;
     Button btSignIn;
     Button btShowPassword;
+    Button btSignUp;
+    boolean isShow = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         etLogin = (EditText) findViewById(R.id.etLogin);
         etPassword = (EditText) findViewById(R.id.etPassword);
         btSignIn = (Button) findViewById(R.id.btSignIn);
+        btSignUp = (Button) findViewById(R.id.btSignUp);
         btShowPassword = (Button) findViewById(R.id.btShowPassword);
 
         btSignIn.setOnClickListener(new View.OnClickListener() {
@@ -49,15 +54,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        btShowPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        btSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFocusChange(View view, boolean b) {
-                if (b)
-                    etPassword.setInputType(InputType.TYPE_TEXT_VARIATION_NORMAL);
+            public void onClick(View view) {
+                String login = "";
+                String password = "";
+
+                // Считываем данные с полей для ввода
+                login = etLogin.getText().toString();
+                password = etPassword.getText().toString();
+
+                if (login.isEmpty()) {
+                    Toast.makeText(MainActivity.this,
+                            "Введите логин для регистрации",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.isEmpty()) {
+                    Toast.makeText(MainActivity.this,
+                            "Введите пароль для регистрации",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                try {
+                    SharedPreferences sPref =
+                            MainActivity.this.getSharedPreferences(Data.SP_KEY_USERS, MODE_PRIVATE);
+                    SharedPreferences.Editor ed = sPref.edit();
+                    ed.putString(Data.SP_KEY_LOGIN, login);
+                    ed.commit();
+                    ed.putString(Data.SP_KEY_PASSWORD, password);
+                    ed.commit();
+                    Toast.makeText(MainActivity.this, "Запись в файл прошла успешно", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Произошла ошибка при записи в файл", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        btShowPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isShow = !isShow;
+
+                if (isShow)
+                    etPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
                 else
                     etPassword.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
             }
         });
     }
+
 }
